7776a4d6156e9dea8a0bc065283f11b12b263e8a
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-08-14 16:41:11 +0600

    Beecrowd

---
+++ 1008 - Salary.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    double N,B,C,D;
+    scanf("%lf",&N);
+    scanf("%lf",&B);
+    scanf("%lf",&C);
+    D=C*B ;
+    printf("NUMBER = %0.0lf\n",N);
+    printf("SALARY = U$ %0.2lf\n",D);
+}
