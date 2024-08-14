601b7ffee26b7133d992694bac3bc6942634ca52
MD. NAWROSE <124398938+MdNawrose@users.noreply.github.com>
2024-08-14 16:39:58 +0600

    Beecrowd

---
+++ 1007 - Difference.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    double a,b,c,d,e,f,g;
+    scanf("%lf",&a);
+    scanf("%lf",&b);
+    scanf("%lf",&c);
+    scanf("%lf",&d);
+    e=a*b;
+    f=c*d;
+    g=e-f;
+    printf("DIFERENCA = %0.0lf\n",g);
+}
