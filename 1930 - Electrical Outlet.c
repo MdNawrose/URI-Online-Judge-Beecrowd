5eb326f76c29ead0d2850423639632ac1fecda27
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2025-01-20 23:24:16 +0600

    Beecrowd

---
+++ 1930 - Electrical Outlet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int a, b, c, d, total;
+
+    scanf("%i %i %i %i", &a, &b, &c, &d);
+
+    total=(a-1)+(b-1)+(c-1)+d;
+
+    printf("%i\n", total);
+
+    return 0;
+}
