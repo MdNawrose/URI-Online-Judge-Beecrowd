720c7122911e8d34a34131591aa11ff467d0abe4
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2025-05-12 02:37:35 +0600

    Beecrowd

---
+++ 2747 - Output 1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    for (i=0; i<39; ++i)
+        printf("-");
+
+    printf("\n");
+
+    printf("|");
+
+    for (i=0; i<37; ++i)
+        printf(" ");
+
+    printf("|\n");
+
+    printf("|");
+
+    for (i=0; i<37; ++i)
+        printf(" ");
+
+    printf("|\n");
+
+    printf("|");
+
+    for (i=0; i<37; ++i)
+        printf(" ");
+
+    printf("|\n");
+
+    printf("|");
+
+    for (i=0; i<37; ++i)
+        printf(" ");
+
+    printf("|\n");
+
+    printf("|");
+
+    for (i=0; i<37; ++i)
+        printf(" ");
+
+    printf("|\n");
+
+    for (i=0; i<39; ++i)
+        printf("-");
+
+    printf("\n");
+
+    return 0;
+}
