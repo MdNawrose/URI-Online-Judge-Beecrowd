6c129246b77c12599854f982a375e2b8c4b9c1fd
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2025-05-23 01:11:44 +0600

    Beecrowd

---
+++ 2755 - Output 9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\"Ro'b'er\tto\\/\"" << endl;
+    cout << "(._.) ( l: ) ( .-. ) ( :l ) (._.)" << endl;
+    cout << "(^_-) (-_-) (-_^)" << endl;
+    cout << "(\"_\") ('.')" << endl;
+    return 0;
+}
