9339103e09714814e4e5b1e8080b93ab6c618627
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2025-06-20 02:28:00 +0600

    Beecrowd

---
+++ 2787 - Chess.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if ((a+b)%2==0)
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+    return 0;
+}
