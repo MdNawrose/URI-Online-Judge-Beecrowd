ab384eceb88aaaf5d52abf0df3d366d3008ee8cb
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2025-07-09 00:35:23 +0600

    Beecrowd

---
+++ 2861 - The Output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    string s;
+    for (int i=0; i<t; ++i)
+    {
+        fflush(stdin);
+        getline(cin, s);
+        cout << "gzuz" << endl;
+    }
+    return 0;
+}
