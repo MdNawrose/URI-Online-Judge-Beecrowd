3a6e2f6f7bf7ed3d3f0930d5e00d2ca617e90690
Md. Nawrose <124398938+MdNawrose@users.noreply.github.com>
2025-07-22 18:58:54 +0600

    Beecrowd

---
+++ 2936 - How Much Cassava.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    long long ans = (a*300)+(b*1500)+(c*600)+(d*1000)+(e*150)+225;
+    cout<<ans<<endl;
+
+    return 0;
+}
